1cf902473bfff31fdcb2b7768ec0fb98718b5328
Athsof1 <athan.sofo@gmail.com>
2020-12-04 03:30:11 +0200

    Hover

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+    $(function () {
+      $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()); //when we reach a certain point
+      })
+    })
+  
