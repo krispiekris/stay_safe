9f800a1850acca5bdc285eeaead2df7b4d3427fb
Mario <mario.stevan@outlook.com>
2020-12-05 18:54:29 +0200

    Works fine

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@
       $(document).scroll(function () {
         var $nav = $("#mainNavbar");
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()); //when we reach a certain point
+         
       })
     })
+    
 
 
 
