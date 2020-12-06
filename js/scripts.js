4c599125c8d91fb437446921d831905c92c376e5
Mario <mario.stevan@outlook.com>
2020-12-06 20:08:56 +0200

    Working LiveStats

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,50 @@
          
       })
     })
+
+
+    // cases
     
+    let new_cases = document.getElementById("new_case");
+let new_death = document.getElementById("new_death");
+let total_death = document.getElementById("total_death");
+let total_recovered = document.getElementById("total_recovered");
+let total_cases = document.getElementById("total_cases");
+let table = document.getElementById('countries_stat')
+// Fetching the Data from the server
+
+//Fetching the World Data
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
+    }
+})
+.then(response => response.json().then( data => {
+    console.log(data);
+    total_cases.innerHTML = data.total_cases;
+    new_cases.innerHTML = data.new_cases;
+    new_death.innerHTML = data.new_deaths;
+    total_death.innerHTML = data.total_deaths;
+    total_recovered.innerHTML = data.total_recovered;
+
+})).catch(err => {
+    console.log(err);
+});
+
+//Fetching The Case by Country Data
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
+    }
+})
+
+.catch(err => {
+    console.log(err);
+});
 
 
   
